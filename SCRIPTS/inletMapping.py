a66384e4858f9624891d94f4779159965e91d2ab
Jie Wang <50101921+JieWangnk@users.noreply.github.com>
2024-07-10 14:47:03 +0000

    update 1

---
+++ SCRIPTS/inletMapping.py
@@ -0,0 +1,120 @@
+import numpy as np
+import re
+import os
+import glob
+
+
+class InletMapping:
+    def __init__(self, center, radius):
+        self.center = np.array(center)
+        self.radius = radius
+
+    def get_face_normal_vectors(self, p1, p2, p3, orientation='in'):
+        v1 = p2 - p1
+        v2 = p3 - p1
+        v3 = np.cross(v1, v2)
+        v3 = v3 / np.linalg.norm(v3)
+        if orientation == 'in' and v3[2] > 0 or orientation == 'out' and v3[2] < 0:
+            v3 = -v3
+        return v3
+
+    def read_csv_file(self, file_name):
+        data = np.genfromtxt(file_name, delimiter=',', skip_header=0)
+        time = data[:, 0]
+        vel = np.transpose(data[:, 1])
+        return time, vel
+
+    def get_velocity_components(self, vel, face_normal_vectors):
+        vel_x = vel * face_normal_vectors[0]
+        vel_y = vel * face_normal_vectors[1]
+        vel_z = vel * face_normal_vectors[2]
+        return vel_x, vel_y, vel_z
+
+    def read_points_file(self, file_name):
+        with open(file_name, 'r') as file:
+            lines = [line.rstrip() for line in file if line.strip()]
+            n_points = int(lines[0])
+            points = []
+            for i in range(n_points):
+                line_s = re.sub('[()]', '', lines[i + 1])
+                point = [float(p) for p in line_s.split()]
+                if point:
+                    points.append(point)
+        return n_points, points
+
+    def get_distance_from_center(self, point):
+        return np.linalg.norm(self.center - point)
+
+    def get_velocity_components_parabolic(self, vel, face_normal_vectors, dist):
+        factor = (1 - (dist / self.radius)**2)
+        return vel * face_normal_vectors * factor
+
+    def write_openfoam_data_format_parabolic(self, file_name, n_points, vel_x_array, vel_y_array, vel_z_array):
+        with open(file_name, 'w') as file:
+            file.write(f"{n_points}\n(\n")
+            for vel_x, vel_y, vel_z in zip(vel_x_array, vel_y_array, vel_z_array):
+                formatted_line = "({:.12f} {:.12f} {:.12f})\n".format(float(vel_x), float(vel_y), float(vel_z))
+                file.write(formatted_line)
+            file.write(")\n")
+
+
+    def write_out(self, inlet_data_file, inlet_name, time, vel, n_points, points, normal_vector, scale=1e0):
+        directory = f"constant/boundaryData/{inlet_name}/{inlet_data_file.split('.')[0]}"
+        parent_dir = os.getcwd()
+        directory = os.path.join(parent_dir, directory)
+        os.makedirs(directory, exist_ok=True)
+
+        for files in glob.glob(directory + '/*'):
+            os.remove(files)
+        
+        if time.size > 0:  # Check if time is not empty
+            for t in time:
+                # print("Processing time:", t)  # Print current time being processed
+                vel_x_array, vel_y_array, vel_z_array = [], [], []
+                for point in points:
+                    point = np.array(point, dtype=float) * scale
+                    dist = self.get_distance_from_center(point)
+                    if dist <= self.radius:
+                        t_idx = np.where(time == t)[0]
+                        if len(t_idx) > 0:  # Check if index exists
+                            velocity_magnitude = vel[t_idx[0]]  # Access velocity magnitude
+                            # print("Velocity magnitude at time", t, ":", velocity_magnitude)
+                            # print("Normal vector:", normal_vector)
+                            # print("Distance from center:", dist)
+                            vel_x, vel_y, vel_z = self.get_velocity_components_parabolic(
+                                velocity_magnitude, normal_vector, dist)
+                            vel_x_array.append(vel_x)
+                            vel_y_array.append(vel_y)
+                            vel_z_array.append(vel_z)
+                        else:
+                            vel_x_array.append(0)
+                            vel_y_array.append(0)
+                            vel_z_array.append(0)
+                    else:
+                        vel_x_array.append(0)
+                        vel_y_array.append(0)
+                        vel_z_array.append(0)
+                self.write_openfoam_data_format_parabolic(
+                    os.path.join(directory, f'U_{t:.6f}'), str(n_points), vel_x_array, vel_y_array, vel_z_array)
+
+    def run(self, inlet_data_file, inlet_name, scale=1e0):
+        # Read the velocity data from the csv file in the INLET folder
+        times, velocities = self.read_csv_file(
+            'constant/boundaryData/{}/{}'.format(inlet_name, inlet_data_file))
+        n_points, points = self.read_points_file(
+            'constant/boundaryData/{}/points'.format(inlet_name))
+        indices = np.random.randint(0, len(points), size=3)
+
+        p1, p2, p3 = points[indices[0]], points[indices[1]], points[indices[2]]
+        normal_vector = self.get_face_normal_vectors(
+            np.asarray(p1), np.asarray(p2), np.asarray(p3), 'out')
+        self.write_out(inlet_data_file, inlet_name,
+                       times, velocities, n_points, points, normal_vector, scale=1e0)
+
+
+# Example usage
+if __name__ == "__main__":
+    processor = InletMapping(
+        center=[-0.0197167, -0.0244641, -0.00713538], radius=0.0055)
+    processor.run("BPM120_U_dopplerU.csv", "inlet")
+
---
+++ userParameter_LL.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+
+REFINEMENT_LEVELS = {
+    "coarse":1,
+    "medium":0.2,
+    "fine":0.1
+}
+# ----------------- #
+# initial condition #
+# ----------------- #
+# define initial condition for U, p, nut
+# 1 inlet, 4 outlets, 1 wall_aorta
+# inlet_type, outlet1_type, outlet2_type, outlet3_type, outlet4_type, wall_aorta_type = self.INITIAL_CONDITION_U.keys()
+
+INITIAL_CONDITION_U = {
+        #"inlet_volumetric_flow_rate": "7.84e-5",
+        "inlet_max_velocity": "0.1",
+        "inlet_normal": "(0, 0, 1)",
+        "inlet_radius": "[-0.015923, -0.0212171, -0.0128499]",
+        "heart_rate": "60",
+        "inlet_center" : "0.0055"
+}
+
+INITIAL_CONDITION_P = {
+        "inlet": "zeroGradient",
+        "outlet": "zeroGradient"
+}
+
+INITIAL_CONDITION_K = {
+        "kInlet": "0.001",
+        "intensityInlet": "0.01"
+}
+
+INITIAL_CONDITION_OMEGA = {
+        "omegaInlet": "0.0001"
+}
+
+SIMULATION_CONTROL = {
+    "startFrom" : "startTime",
+    "startTime" : "0",
+    "stopAt" : "endTime",
+    "endTime" : "1",
+    "deltaT" : "0.001",
+    "writeControl" : "runTime",    #timeStep, runTime, adjustableRunTime
+    "writeInterval" : "0.1",       # 1, 0.1, 0.01
+    "runTimeModifiable" : "true",
+    "functionList" : ["wallShearStress"]
+}
+
+WK_SETTING = {
+    "A_rcca" : "2.880E-05",  # RCCA area
+    "A_lcca" : "1.089E-05",  # LCCA area
+    "A_lsca" : "1.827E-05",  # LSCA area
+    "A_DAo" : "3.944E-05",   # DAo area
+    "percentage" : "40",  # Insert percentage flow through branches (30% average) 
+    "SP" : "113",  # Systole Pressure   
+    "DP" : "62",   # Dystole Pressure     
+    "HR" : "120"  # Heart Rate
+}
+
+
+
+
