d6a7fa4bccc488a04f115c0170bba5a23a3b7e1b
JieWangnk <jieandwang@gmail.com>
2025-01-27 10:23:33 +0000

    userParameter

--- userParameter_HL.py
+++ userParameter_HL.py
@@ -1,6 +1,6 @@
 from userParameter_LL import *
 
-GEOMETRY_CASE = "VOL04_plug"
+GEOMETRY_CASE = "VOL04"
 REFINEMENT = "coarse" #options: "coarse", "medium", "fine"
 GEOMETRY_SCALE = "1e-3"
 #-------------------------------------------------------------------------------------------#
@@ -12,7 +12,7 @@ BC_OUTLET = "3EWINDKESSEL" #options: ZERO_GRADIENT, 3EWINDKESSEL
 # Inlet details
 INLET_DATA_FILE = "BPM73.csv"
 INLET_PROFILE = "parabolic" #options: plug, womersley, parabolic
-INLET_ORINTATION = "out" # Often is out!!! options: in or out, as normal vector up to z-axis is out, down to z-axis is in 
+INLET_ORINTATION = "in" # Often is out!!! options: in or out, as normal vector up to z-axis is out, down to z-axis is in 
 #-------------------------------------------------------------------------------------------#
 # Physical properties: transportProperteis and momentumTransport
 NU = "3.3e-06"
